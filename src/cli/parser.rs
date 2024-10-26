908999bfa2a3afea09d30f674c6636b750c6ecb7
LeweLotki <stefanczyk@gmail.com>
2024-10-26 17:39:44 +0200

    adjusting logic in parser

--- src/cli/parser.rs
+++ src/cli/parser.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use clap::CommandFactory;
+
 use crate::modes::ContentManager;
 use crate::modes::dictionary::Dictionary;
 use crate::modes::hasher::Hasher;
@@ -21,38 +23,60 @@ impl CLI {
     pub fn run_parser() {
         let args = Self::parse();
 
-        if args.hash.is_some() && args.dictionary.is_none() {
-            println!("Warning: Please provide a dictionary file using --dictionary.");
-            return;
-        }
+        let dictionary_flag = args.dictionary.is_some();
+        let hash_flag = args.hash.is_some();
+        let passwords_flag = args.passwords.is_some();
 
-        let mut passwords: Option<Passwords> = None;
-        if let Some(ref passwords_path) = args.passwords {
-            let mut loaded_passwords = Passwords::new(passwords_path.clone());
-            loaded_passwords.load();
-            loaded_passwords.display();
-            passwords = Some(loaded_passwords);
-        }
-
-        let mut hasher: Option<Hasher> = None;
-        if let Some(ref dictionary_path) = args.dictionary {
-            let mut dictionary = Dictionary::new(dictionary_path.clone());
-            dictionary.load();
-
-            if args.hash.is_none() {
+        if dictionary_flag && !hash_flag && !passwords_flag {
+            if let Some(ref dictionary_path) = args.dictionary {
+                let mut dictionary = Dictionary::new(dictionary_path.clone());
+                dictionary.load();
                 dictionary.display();
-            } else if let Some(ref hash_function) = args.hash {
-                let mut created_hasher = Hasher::new(dictionary, hash_function.to_string());
-                created_hasher.load();
-                created_hasher.display();
-                hasher = Some(created_hasher);
+                return;
             }
         }
 
-        if let (Some(hasher), Some(passwords)) = (hasher, passwords) {
-            let retriver = Retriver::new(hasher, passwords);
-            retriver.run();
+        if hash_flag && !dictionary_flag && !passwords_flag {
+            if let Some(ref hash_function) = args.hash {
+                let dummy_dictionary = Dictionary::new(String::new()); 
+                let mut hasher = Hasher::new(dummy_dictionary, hash_function.clone());
+                hasher.load();
+                hasher.display();
+                return;
+            }
         }
+
+        if passwords_flag && !dictionary_flag && !hash_flag {
+            if let Some(ref passwords_path) = args.passwords {
+                let mut passwords = Passwords::new(passwords_path.clone());
+                passwords.load();
+                passwords.display();
+                return;
+            }
+        }
+
+        if dictionary_flag && hash_flag && passwords_flag {
+            if let (Some(ref dictionary_path), Some(ref hash_function), Some(ref passwords_path)) =
+                (args.dictionary, args.hash, args.passwords)
+            {
+                let mut dictionary = Dictionary::new(dictionary_path.clone());
+                dictionary.load();
+
+                let mut passwords = Passwords::new(passwords_path.clone());
+                passwords.load();
+
+                let mut hasher = Hasher::new(dictionary, hash_function.clone());
+                hasher.load();
+
+                let retriver = Retriver::new(hasher, passwords);
+                retriver.run();
+                return;
+            }
+        }
+
+        println!("Error: Wrong flags combination.");
+        CLI::command().print_help().unwrap();
+        println!(); 
     }
 }
 
