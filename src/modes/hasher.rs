0da6cf0b92f1c981a394fdcbbae298290c65812b
LeweLotki <stefanczyk@gmail.com>
2024-10-27 15:07:19 +0100

    parrallel hashing

--- src/modes/hasher.rs
+++ src/modes/hasher.rs
@@ -5,6 +5,8 @@ use hex;
 use crate::modes::dictionary::Dictionary;
 use crate::modes::ContentManager;
 
+use rayon::prelude::*;
+
 pub struct Hasher {
     pub dictionary: Dictionary,
     pub hashes: Vec<String>,
@@ -13,47 +15,52 @@ pub struct Hasher {
 }
 
 impl Hasher {
-   pub fn new(dictionary: Dictionary, hash_function: String) -> Self {
+    pub fn new(dictionary: Dictionary, hash_function: String) -> Self {
         Self {
             dictionary,
             hashes: Vec::new(),
             tokens: Vec::new(),
-            hash_function
+            hash_function,
         }
     }
 
-    pub fn load_hashes(&mut self) -> () {
-        let tokens: Vec<String> = self.dictionary.tokens.iter().cloned().collect();
-        match self.hash_function.as_str() {
-            "sha256" => {
-                for token in tokens {
-                    let hasher = Sha256::new();
-                    self.add_hash(token.to_string(), hasher);
+    pub fn load_hashes(&mut self) {
+        let (tokens, hashes) = {
+            let tokens_ref = &self.dictionary.tokens;
+
+            match self.hash_function.as_str() {
+                "sha256" => {
+                    self.hash_tokens_in_parallel::<Sha256>(tokens_ref)
+                }
+                "sha512" => {
+                    self.hash_tokens_in_parallel::<Sha512>(tokens_ref)
+                }
+                "md5" => {
+                    self.hash_tokens_in_parallel::<Md5>(tokens_ref)
+                }
+                _ => {
+                    println!("Unsupported hash function: {}", self.hash_function);
+                    (Vec::new(), Vec::new())
                 }
             }
-            "sha512" => {
-                for token in tokens {
-                    let hasher = Sha512::new();
-                    self.add_hash(token.to_string(), hasher);
-                }
-            }
-            "md5" => {
-                for token in tokens {
-                    let hasher = Md5::new();
-                    self.add_hash(token.to_string(), hasher);
-                }
-            }
-            _ => {
-                println!("Unsupported hash function: {}", self.hash_function);
-            }
-        }
+        };
+
+        self.tokens = tokens;
+        self.hashes = hashes;
     }
 
-    fn add_hash<H: Digest>(&mut self, token: String, mut hasher: H) {
-        hasher.update(token.as_bytes());
-        let result = hasher.finalize();
-        self.tokens.push(token.clone());
-        self.hashes.push(hex::encode(result));
+    fn hash_tokens_in_parallel<H>(&self, tokens: &Vec<String>) -> (Vec<String>, Vec<String>)
+    where
+        H: Digest + Send + Sync + 'static,
+    {
+        tokens
+            .par_iter()
+            .map(|token| {
+                let hasher = H::new();
+                let hash = compute_hash(token, hasher);
+                (token.clone(), hash)
+            })
+            .unzip()
     }
 
     pub fn display_hashes(&self) {
@@ -63,12 +70,18 @@ impl Hasher {
     }
 }
 
+fn compute_hash<H: Digest>(token: &str, mut hasher: H) -> String {
+    hasher.update(token.as_bytes());
+    let result = hasher.finalize();
+    hex::encode(result)
+}
+
 impl ContentManager for Hasher {
-    fn load(&mut self) -> () {
-       self.load_hashes(); 
+    fn load(&mut self) {
+        self.load_hashes();
     }
 
-    fn display(&self) -> () {
+    fn display(&self) {
         match self.hash_function.as_str() {
             "sha256" => {
                 println!("Hash Function: SHA-256");
@@ -89,3 +102,4 @@ impl ContentManager for Hasher {
         }
     }
 }
+
